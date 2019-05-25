1937a18aba48e3e8ef8449d2fa41a63f1ef597bc
kindcao <kind790@163.com>
2019-05-25 16:01:01 +0800

    修改项目名称

--- src/router/route.js
+++ src/router/route.js
@@ -53,7 +53,7 @@ export const appRouter = [
   {
     path: '/EngineeringManagement',
     name: 'EngineeringManagement',
-    title: '工程管理kind',
+    title: '工程管理kind2',
     meta: {
       requireAuth: true
     },
